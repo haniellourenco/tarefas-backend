5c1a204568766e9b048dc4c58153ce21343fa86d
Haniel <hanielohn2016@gmail.com>
2023-05-04 21:40:13 -0300

    add: cadastrar tarefa

--- db.js
+++ db.js
@@ -1,70 +1,74 @@
 const mysql = require("mysql2/promise");
 
 // função que cria uma conexão com o banco
-async function connect(){
-    // verificar se já tem objeto de conexao na seção de uso
-    if(global.connection && global.connection.state !== 'disconnected'){
-        return global.connection;
-    }
+async function connect() {
+	// verificar se já tem objeto de conexao na seção de uso
+	if (global.connection && global.connection.state !== "disconnected") {
+		return global.connection;
+	}
 
-    //cria uma conexao
-    const conexao = await mysql.createConnection({
-        host : 'localhost',
-        port : 3306,
-        user : 'root',
-        password : '',
-        database : 'tarefas'
-    });
+	//cria uma conexao
+	const conexao = await mysql.createConnection({
+		host: "localhost",
+		port: 3306,
+		user: "root",
+		password: "",
+		database: "tarefas",
+	});
 
-    // avisa no console
-    console.log("Conexao com o BD foi criada")
+	// avisa no console
+	console.log("Conexao com o BD foi criada");
 
-    //guarda objeto de conexao para a seção de uso
-    global.connection = conexao;
+	//guarda objeto de conexao para a seção de uso
+	global.connection = conexao;
 
-    // retorna a conexao com o bd
-    return global.connection;
+	// retorna a conexao com o bd
+	return global.connection;
 }
 
 // função que busca as tarefas
-async function recuperarTarefas(){
-    // pega ou cria uma conexão
-    const con = await connect();
+async function recuperarTarefas() {
+	// pega ou cria uma conexão
+	const con = await connect();
 
-    const query = `SELECT tarefas.tarefatexto AS tarefa, GROUP_CONCAT(subtarefas.subtarefatexto) AS subtarefas FROM tarefas INNER JOIN subtarefas ON tarefas.id = subtarefas.tarefa_id GROUP BY tarefas.id, tarefas.tarefatexto`;
+	const query = `SELECT tarefas.tarefatexto AS tarefa, GROUP_CONCAT(subtarefas.subtarefatexto) AS subtarefas FROM tarefas INNER JOIN subtarefas ON tarefas.id = subtarefas.tarefa_id GROUP BY tarefas.id, tarefas.tarefatexto`;
 
-    const [results] = await con.query(query)
+	const [results] = await con.query(query);
 
-    const json = results.map((result) => {
-        const { tarefa, subtarefas } = result;
-        return { tarefa, subtarefas: subtarefas.split(',') };
-      });
-    
-    // retorna o json da consulta
-    return json
+	const json = results.map((result) => {
+		const { tarefa, subtarefas } = result;
+		return { tarefa, subtarefas: subtarefas.split(",") };
+	});
+
+	// retorna o json da consulta
+	return json;
 }
 
-async function recuperarTarefa(id){
-    // pega ou cria uma conexão
-    const con = await connect();
+async function recuperarTarefa(id) {
+	// pega ou cria uma conexão
+	const con = await connect();
 
-    const query = `SELECT tarefas.tarefatexto AS tarefa, GROUP_CONCAT(subtarefas.subtarefatexto) AS subtarefas FROM tarefas INNER JOIN subtarefas ON tarefas.id = subtarefas.tarefa_id WHERE tarefas.id = ${id} GROUP BY tarefas.id, tarefas.tarefatexto`;
+	const query = `SELECT tarefas.tarefatexto AS tarefa, GROUP_CONCAT(subtarefas.subtarefatexto) AS subtarefas FROM tarefas INNER JOIN subtarefas ON tarefas.id = subtarefas.tarefa_id WHERE tarefas.id = ${id} GROUP BY tarefas.id, tarefas.tarefatexto`;
 
-    const [results] = await con.query(query)
+	const [results] = await con.query(query);
 
-    const json = results.map((result) => {
-        const { tarefa, subtarefas } = result;
-        return { tarefa, subtarefas: subtarefas.split(',') };
-      });
-    
-    // retorna o json da consulta
-    return json
+	const json = results.map((result) => {
+		const { tarefa, subtarefas } = result;
+		return { tarefa, subtarefas: subtarefas.split(",") };
+	});
+
+	// retorna o json da consulta
+	return json;
 }
 
 // aqui vao outras funções de manipulação do BD
-
+async function cadastrarTarefa(tarefa) {
+	const con = await connect();
+	const query = "INSERT INTO tarefas(tarefatexto) VALUES (?);";
+	return await conn.query(query, [tarefas.tarefatexto]);
+}
 
 // executa a função connect
 connect();
 //exporta funções
-module.exports = { recuperarTarefas, recuperarTarefa }
+module.exports = { recuperarTarefas, recuperarTarefa };
